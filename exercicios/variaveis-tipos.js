d40c64aae0302df32448c6cb7aea29f77209024b
Saulorica <saulorica@gmail.com>
2023-06-12 16:26:52 -0400

    conteudo das aulas

---
+++ exercicios/variaveis-tipos.js
@@ -0,0 +1,11 @@
+let nome = "Saulo"; //string.
+let idade = "38"; //number.
+let nota; //underfined *quando o valor da variavel nao esta definido.
+//nota = null //null *quando o valor ainda não esta definido ou é nulo.
+// não é aplicado crase, porque null é uma palavra reservada por sí só.
+
+console.log(nota); //undefined
+nota = 10;
+console.log(nota); //10
+nota = null;
+console.log(nota); //null
