b0049fe1311d25136da9a4d80a85877a88c3c775
Saulorica <saulorica@gmail.com>
2023-06-13 20:36:09 -0400

    conteudo da aula

---
+++ exercicios/variaveis-x++e++x.js
@@ -0,0 +1,20 @@
+let x = 10;
+// o ++x emcrementa +1 ao valor da variavel, 
+// x++;
+// console.log(x); // 11
+
+// quando o sinal vem na frente, ele soma primeiro e depois retorna 
+let y = ++x;
+console.log("x", x); // 11
+console.log("y", y); // 11 
+
+
+// x--;
+// console.log(x) // 9
+// o --x subtrai 1 do valor da variavel
+
+// quando o sinal vem na atras, ele retorna primeiro e depois ele soma
+
+// let y = x++;
+// console.log("x", x); // 11
+// console.log("y", y); // 10
