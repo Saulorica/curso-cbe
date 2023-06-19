c0e92dec1ac7387285e365b9662e9c5389237a0e
Saulorica <saulorica@gmail.com>
2023-06-19 16:57:26 -0400

    Aula de Condicionais

---
+++ Condicionais/operadores-de-comparacao.js
@@ -0,0 +1,15 @@
+// Buleano é o tipo de dado que guarda verdadeiro ou falso;
+
+const ehMaior = 25 >= 16;
+console.log(ehMaior); // true
+
+// Maior que (>) 5 > 3;
+// Maior ou igual (>=) 5 >= 5;
+// Menor que (<) 4 < 6;
+// Menor ou igual (<=) 4 <= 4;
+// Igual (==) 3 == "3" , 2 == 2;
+// retorna verdadeiro independente do tipo.
+// Diferente (!=) 3 != 4 , 4 != "3":
+// Estritamente verdadeiro (===) 3 === 3, "a" === "a";
+// Retorna verdadeiro quando são iguais e do mesmo tipo.
+// Estritamente diferente (!==) 3 !== "3", 2 !== 3:
